0640787b3c974d6c53304b458d235c294f360ea7
oldyoungcoders <132730577+oldyoungcoders@users.noreply.github.com>
2023-11-01 19:46:25 +0800

    Add files via upload
    lab7

---
+++ ap.js
@@ -0,0 +1,185 @@
+"use strict"; 
+//for -> davtalt
+// for(ehlel,tugsgul,nemgedeh uu? hasagdah uu?){davtah kodoo bichne}
+// for (var s = 1 ; s<=5; s++){
+//     console.log(s)
+//     console.log("Hi")
+// }
+// i=1 1<=5 ? true 1 hi
+// i=2 2<=5 ? true 2 hi
+// ingej aivagdana.
+
+// for (var l = 10; l>1 ; l--){
+//     console.log(l)
+// }
+
+// for(var p = 1 ; p <= 20 ; p++){
+//     if(p%2==0){
+//         console.log(p )
+//     } else {
+//         console.log("")
+//     }
+// }
+
+
+// for(var i = 1 ; i <= 20 ; i++){
+//     if(i%2==0){
+//         console.log( "tegsh "+i )
+//     } else {
+//         console.log("sondgoi " + i)
+//     }
+// }
+
+
+
+// var sum = 0
+// for (var o = 1 ; o <= 10 ; o ++) {
+//     sum = sum + o ; 
+//     console.log(sum)
+// }
+
+
+
+// var su = 1
+// for (var oo = 1 ; oo <= 10 ; oo ++) {
+//     su = su * oo ; 
+//     console.log(su)
+// }
+
+
+
+// for(var pe = 1 ; pe <= 100 ; pe++ ){
+//     if(pe%3==0 && pe%5==0){
+//         console.log(pe )
+//     } else {
+
+//     }
+// }
+
+
+
+// var niilber = 0 , lber2=1;
+// var n = prompt("Too oruul.");
+// for (var i =1 ; i<=n;i++){
+//     niilber = niilber + 100;
+//     lber2 = n*100;
+// }
+
+// console.log(niilber)
+// console.log(lber2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var random = Math.floor(Math.random()*10)+1;
+// console.log(random);
+
+// var too = +prompt("Too oruulna uu.") ;
+// var chance=3;
+
+//     if (random == too){
+//         alert("Ta taalaa.");
+
+//     } else if (random < too ) {
+//         alert("Arai ih baina.");
+//         +prompt("Too oruulna uu.") ;
+            
+//      } else if (random > too) {
+//         +prompt("Too oruulna uu.") ;
+//         alert("Arai baga baina.");
+        
+//     } else if (too>chance) {
+//         alert ("Ta hojigdav. ");
+
+//     } else if (chance - 3){
+//         alert ("Ta 3 deh udaa deeree yallaa");
+
+//     } else if (too  =  chance -1) {
+//         alert("Ta anhnii oroldlogooroo aillaa.");
+
+//     } else if (too = chance -2) {
+//         alert ("Ta 2 deh udaa deree yallaa")  ;   
+        
+//     } 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
